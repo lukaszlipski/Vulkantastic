129802ad52e9a75d13aa7dfc584f62ae08ec5211
lukaszlipski <lukasz.lipski@hotmail.com>
2018-09-16 21:42:10 +0200

    Attempt to fix path creation inside CompileShaders.py

--- Vulkantastic/Scripts/CompileShaders.py
+++ Vulkantastic/Scripts/CompileShaders.py
@@ -1,16 +1,17 @@
 import os
 from subprocess import call
 
-CurrentPath = os.path.dirname(__file__)
+CurrentPath = os.path.dirname(os.path.abspath(__file__))
 
-SrcPath = os.path.join(CurrentPath, "../Source/Shaders/")
-DstPath = os.path.join(CurrentPath, "../Shaders/")
+SrcPath = os.path.normpath(os.path.join(CurrentPath, "../Source/Shaders/"))
+DstPath = os.path.normpath(os.path.join(CurrentPath, "../Shaders/"))
 
 Files = os.listdir(SrcPath)
 
 for File in Files:
     FileName, FileExtension = os.path.splitext(File)
-    SrcPathWithName = SrcPath + File
-    DstPathWithName = DstPath + FileName + FileExtension + ".spv "
-    Command = "glslangValidator.exe -V -o " + DstPathWithName + SrcPathWithName
-    call(Command)
+    SrcPathWithName = os.path.join(SrcPath,File)
+    DstPathWithName = os.path.join(DstPath, FileName + FileExtension + ".spv")
+    Command = "glslangValidator.exe -V -o " + DstPathWithName + " " + SrcPathWithName
+    print(Command)
+    call(str(Command))
